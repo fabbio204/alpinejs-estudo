cb10cc59be9f72d3a83675c0469b4b2a877001e5
Fábio Lima da Silva <fabbiolimmadasilva@gmail.com>
2023-02-22 08:45:38 -0300

    instalação do tailwindcss

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,38 @@ module.exports = {
         index: './src/index.js'
     },
     output: {
-        filename: '[name].js',
+        filename: 'bundle.js',
         path: path.resolve(__dirname, 'dist'),
+    },
+    devServer: {
+        static: {
+            directory: path.resolve(__dirname, 'dist'),
+        },
+        port: 3000,
+        open: true,
+        hot: true,
+        compress: true,
+        historyApiFallback: true,
+    },
+    module: {
+        rules: [
+            {
+                test: /\.html$/,
+                use: [
+                    {
+                        loader: 'html-loader',
+                        options: {
+                            minimize: true,
+                            interpolation: false
+                        }
+                    }
+                ]
+            },
+            {
+                test: /\.css$/i,
+                include: path.resolve(__dirname, 'src'),
+                use: ['style-loader', 'css-loader', 'postcss-loader'],
+            },
+        ],
     },
 };
